3bc585555f9a94cad5cbc5fe6b037e6fb38f6efc
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2024-08-07 18:13:36 -0700

    Misc tests of Reanimated's layout API

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,102 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {useState} from 'react';
+import {Image, StyleSheet, Platform, View, Pressable, Text} from 'react-native';
+import Reanimated, {LinearTransition} from 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {HelloWave} from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const styles = StyleSheet.create({
+  wrapper: {
+    ...StyleSheet.absoluteFillObject,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+  animatedBox: {
+    backgroundColor: 'red',
+    borderRadius: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    padding: 4,
+  },
+  stateA: {
+    top: 0,
+    width: 100,
+    height: 100,
+  },
+  stateB: {
+    width: 300,
+    height: 200,
+  },
+  leftBox: {
+    borderTopLeftRadius: 12,
+    borderBottomLeftRadius: 12,
+  },
+  rightBox: {
+    borderTopRightRadius: 12,
+    borderBottomRightRadius: 12,
+  },
+  child: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  childMarginA: {
+    margin: 4,
+  },
+  childMarginB: {
+    margin: 2,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 100,
+    width: 100,
+    height: 50,
+    marginTop: 80,
+    backgroundColor: 'green',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'white',
+  },
+  bufferBox: {
+    width: 100,
+    height: 20,
+    marginTop: 40,
+    backgroundColor: 'green',
+  },
+});
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+const transition = LinearTransition.springify().mass(0.5).damping(10).stiffness(100);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function HomeScreen() {
+  const [state, setState] = useState(false);
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <View style={styles.wrapper}>
+      <Reanimated.View style={[styles.animatedBox, state ? styles.stateB : styles.stateA]} layout={transition}>
+        <Reanimated.View
+          style={[styles.leftBox, styles.child, state ? styles.childMarginB : styles.childMarginA]}
+          layout={transition}
+        />
+        <Reanimated.View
+          style={[styles.rightBox, styles.child, state ? styles.childMarginB : styles.childMarginA]}
+          layout={transition}
+        />
+      </Reanimated.View>
+      <Reanimated.View style={styles.bufferBox} layout={transition} />
+      <Pressable onPress={() => setState(!state)} style={styles.button}>
+        <Text style={styles.text}>Toggle</Text>
+      </Pressable>
+    </View>
   );
 }
